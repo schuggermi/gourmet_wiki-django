1f580ca4c45cd9119eb3435d78ae71c55cb7490f
schuggermi <mschuchowski@gmx.de>
2025-07-04 10:57:41 +0200

    changes

--- menus/views.py
+++ menus/views.py
@@ -3,14 +3,30 @@ from pathlib import Path
 from django.conf import settings
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
+from django.template.loader import render_to_string
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView
 from formtools.wizard.views import SessionWizardView
 
-from menus.forms import MenuForm
-from menus.models import Menu
+from menus.forms import MenuForm, MenuCourseForm
+from menus.formsets import MenuCourseFormSet
+from menus.models import Menu, MenuCourse
 from recipes.utils import calculate_scaled_ingredients_menu
+
+
+def add_menu_course_form(request):
+    form_index = int(request.GET.get("form_count", 0))
+    new_form = MenuCourseForm(prefix=f'menu_course-{form_index}')
+
+    context = {
+        'form': new_form,
+        'form_index': form_index,
+    }
+
+    html = render_to_string('menus/partials/menu_course_form_row.html', context)
+    return HttpResponse(html)
 
 
 class MenuListView(ListView):
@@ -35,6 +51,7 @@ class MenuDetailView(DetailView):
 class CreateMenuWizardView(LoginRequiredMixin, SessionWizardView):
     form_list = [
         ('0', MenuForm),
+        ('1', MenuCourseFormSet),
     ]
     template_name = 'menus/create_menu_wizard.html'
     file_storage = FileSystemStorage(location=Path(settings.MEDIA_ROOT).joinpath('menus/images/temp'))
@@ -45,7 +62,7 @@ class CreateMenuWizardView(LoginRequiredMixin, SessionWizardView):
         self.menu_instance = None
 
     def dispatch(self, request, *args, **kwargs):
-        self.menu_id = kwargs.get('recipe_id')
+        self.menu_id = kwargs.get('menu_id')
         if self.menu_id:
             self.menu_instance = get_object_or_404(Menu, id=self.menu_id, created_by=request.user.pk)
         return super().dispatch(request, *args, **kwargs)
@@ -65,6 +82,16 @@ class CreateMenuWizardView(LoginRequiredMixin, SessionWizardView):
             if self.menu_instance:
                 kwargs['instance'] = self.menu_instance
             return MenuForm(**kwargs)
+        elif step == '1':
+            if self.menu_instance:
+                queryset = self.menu_instance.get_course_types
+            else:
+                queryset = MenuCourse.objects.none()
+            kwargs.update({
+                'prefix': 'menu_course',
+                'queryset': queryset,
+            })
+            return MenuCourseFormSet(**kwargs)
         return super().get_form(step, data, files)
 
     def done(self, form_list, **kwargs):
@@ -75,6 +102,7 @@ class CreateMenuWizardView(LoginRequiredMixin, SessionWizardView):
         )
 
         if not menu_form.is_valid():
+            print("Menu form validation failed")
             return self.render_revalidation_failure(step='0', form=menu_form)
 
         if self.menu_instance:
@@ -83,5 +111,23 @@ class CreateMenuWizardView(LoginRequiredMixin, SessionWizardView):
             menu = menu_form.save(commit=False)
             menu.created_by = self.request.user
             menu.save()
+
+        # Step 1 – Menu Courses
+        courses_formset = self.get_form(step='1', data=self.storage.get_step_data('1'))
+        if not courses_formset.is_valid():
+            print("Courses formset validation failed")
+            return self.render_revalidation_failure(step='1', form=courses_formset)
+
+        for index, form in enumerate(courses_formset):
+            if form.data.get(f"{form.prefix}-DELETE", False):
+                if form.instance.pk:
+                    form.instance.delete()
+            else:
+                instance = form.save(commit=False)
+                if not instance.pk:
+                    instance.menu = menu
+                instance.order = form.cleaned_data.get('order', index)
+                instance.course_type = form.cleaned_data['course_type']
+                instance.save()
 
         return redirect(reverse('menu_detail', kwargs={'pk': menu.id}))
