cb65bc60034049988ef6a64a6cfbefb96f14c746
schuggermi <mschuchowski@gmx.de>
2025-07-06 14:26:53 +0200

    changes

--- menus/forms.py
+++ menus/forms.py
@@ -9,6 +9,20 @@ class MenuForm(forms.ModelForm):
     class Meta:
         model = Menu
         fields = ['name', 'description', 'portions']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Give your menu a name (e.g. Weekend Dinner Special)',
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'rows': 3,
+                    'cols': 40,
+                    'placeholder': 'e.g. A curated selection of comforting dishes for a cozy weekend dinner.',
+                }
+            ),
+        }
 
 
 class MenuCourseForm(forms.ModelForm):
@@ -21,12 +35,6 @@ class MenuCourseForm(forms.ModelForm):
 
     def __init__(self, *args, user=None, **kwargs):
         super().__init__(*args, **kwargs)
-
-        # Filter recipes to only show those created by or favorited by the user
-        if user:
-            self.fields['recipe'].queryset = Recipe.objects.filter(
-                Q(created_by=user) | Q(favorite_by=user)
-            ).distinct()
 
         for field_name in self.fields:
             if self.errors.get(field_name):
