5c5067127ca66e47bd25c7615654bba82023aa97
schuggermi <mschuchowski@gmx.de>
2025-07-26 06:43:00 +0200

    fix new setup

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-if [ "$DATABASE" = "$POSTGRES_DB" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $POSTGRES_HOST $POSTGRES_PORT; do
-      sleep 0.1
-    done
-
-    echo "PostgreSQL started"
-fi
+wait-for-it.sh "$POSTGRES_HOST":"$POSTGRES_PORT" -- echo "PostgreSQL started"
 
 python ./manage.py makemigrations --settings=gourmet_wiki.settings
 python ./manage.py migrate --settings=gourmet_wiki.settings
