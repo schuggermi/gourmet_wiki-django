d74b16add2885b7f33379f3ac149c5f28a71455b
schuggermi <mschuchowski@gmx.de>
2025-07-26 04:47:17 +0200

    included combined setup in docker for web and emt

--- app/gourmet_wiki/settings.py
+++ app/gourmet_wiki/settings.py
@@ -128,10 +128,18 @@ WSGI_APPLICATION = 'gourmet_wiki.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('WEB_POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    },
 }
 
 # Password validation
