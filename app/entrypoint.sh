f707821a5c96d1cc0862ef598d38e8cb367ca46d
schuggermi <mschuchowski@gmx.de>
2025-08-01 15:29:03 +0200

    fix

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 echo "Waiting for PostgreSQL at $POSTGRES_HOST:$POSTGRES_PORT..."
 wait-for-it.sh -h "$POSTGRES_HOST" -p "$POSTGRES_PORT" -t 30 -- echo "PostgreSQL started"
 
+npm run build
+
 python manage.py makemigrations --settings=gourmet_wiki.settings
 python manage.py migrate --settings=gourmet_wiki.settings
 python manage.py collectstatic --settings=gourmet_wiki.settings --no-input --clear
