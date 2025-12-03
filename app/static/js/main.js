45838750a59818fe4b08f28d1f96a588d1aff3bf
schuggermi <mschuchowski@gmx.de>
2025-12-03 12:13:56 +0100

    changes

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -6,11 +6,11 @@ import {reindexFormset} from './formset-utils';
 window.Sortable = Sortable;
 window.reindexFormset = reindexFormset;
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.documentElement.setAttribute('data-theme', 'dark');
-} else {
-    document.documentElement.setAttribute('data-theme', 'light');
-}
+// if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+//     document.documentElement.setAttribute('data-theme', 'dark');
+// } else {
+//     document.documentElement.setAttribute('data-theme', 'light');
+// }
 
 // Listen for system theme changes
 //window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
