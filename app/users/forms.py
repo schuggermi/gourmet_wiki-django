1895b65d681f0b4782a550f37e1938d4004b6118
schuggermi <mschuchowski@gmx.de>
2026-01-08 19:45:17 +0100

    fix signup form

--- app/users/forms.py
+++ app/users/forms.py
@@ -1,20 +1,38 @@
 from allauth.account.forms import SignupForm
 from django import forms
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+
+User = get_user_model()
 
 
 class CustomSignupForm(SignupForm):
     first_name = forms.CharField(
-        max_length=50,
-        label='First Name',
+        max_length=User._meta.get_field('first_name').max_length,
+        label=User._meta.get_field('first_name').verbose_name,
         widget=forms.TextInput(attrs={
-            'placeholder': 'First Name',
+            'placeholder': 'Paul',
         })
     )
     last_name = forms.CharField(
-        max_length=50,
-        label='Last Name',
+        max_length=User._meta.get_field('last_name').max_length,
+        label=User._meta.get_field('last_name').verbose_name,
         widget=forms.TextInput(attrs={
-            'placeholder': 'Last Name',
+            'placeholder': 'Bocuse',
+        })
+    )
+    email = forms.EmailField(
+        max_length=User._meta.get_field('email').max_length,
+        label=User._meta.get_field('email').verbose_name,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'paul.bocuse@example.com',
+        })
+    )
+    username = forms.CharField(
+        max_length=User._meta.get_field('username').max_length,
+        label=User._meta.get_field('username').verbose_name,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'paul.bocuse',
         })
     )
 
