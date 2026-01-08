0f1d266ea66ae78626d919daf81c55c041d00bcd
schuggermi <mschuchowski@gmx.de>
2026-01-08 19:28:26 +0100

    deactivate require login middleware

--- app/gourmet_wiki/settings.py
+++ app/gourmet_wiki/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'core.middleware.RequireLoginMiddleware',
+    #'core.middleware.RequireLoginMiddleware',
     'allauth.account.middleware.AccountMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'users.middleware.HTMXLoginRedirectMiddleware',
